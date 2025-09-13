7c427c258b3fa167cbfdfe1f3a37dc912a9359ad
Raphael Ramos <raphael.opensource@gmail.com>
2025-09-13 20:40:56 -0300

    refactor: encapsulate darknet detection routine

--- src/notebooks/utils.py
+++ src/notebooks/utils.py
@@ -1,10 +1,21 @@
+import os
 import cv2
 import matplotlib.pyplot as plt
 
-def mostrar_imagem(caminho):
+DARKNET_PATH = "../../shared/darknet"
+
+
+def mostrar_imagem(caminho: str) -> None:
     imagem = cv2.imread(caminho)
     fig = plt.gcf()
-    fig.set_size_inches(18,10)
-    plt.axis('off')
+    fig.set_size_inches(18, 10)
+    plt.axis("off")
     plt.imshow(cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB))
     plt.show()
+
+
+def detectar_objetos(caminho: str) -> None:
+    os.system(
+        f"cd {DARKNET_PATH} && ./darknet detect cfg/yolov3.cfg ../yolov3.weights {caminho}"
+    )
+    mostrar_imagem(caminho=f"{DARKNET_PATH}/predictions.jpg")
