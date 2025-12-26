0989eec44bc65fdb3d831b59119dbd2718c36be1
Raphael Ramos <raphael.opensource@gmail.com>
2025-12-26 18:32:04 -0300

    feat: finish YOLO cli implementations

--- src/helpers/yolo.py
+++ src/helpers/yolo.py
@@ -1,11 +1,15 @@
 import os
 from .media import buscar_imagem
 
-def detectar_objetos(imagem: str) -> None:
+def detectar_objetos(imagem: str, conf: float = 0.25) -> None:
     imagem = buscar_imagem(imagem)
-    yolo_cmd = f"yolo task=detect mode=predict model=yolov8n.pt conf=0.25 source='{imagem}' save=True"
+    yolo_cmd = f"yolo task=detect mode=predict model=yolov8n.pt conf={conf} source='{imagem}' save=True"
     os.system(yolo_cmd)
 
-def detectar_objetos_url(url_imagem: str) -> None:
-    yolo_cmd = f"yolo task=detect mode=predict model=yolov8n.pt conf=0.25 source='{url_imagem}' save=True"
+def detectar_objetos_url(url_imagem: str, conf: float = 0.25) -> None:
+    yolo_cmd = f"yolo task=detect mode=predict model=yolov8n.pt conf={conf} source='{url_imagem}' save=True"
+    os.system(yolo_cmd)
+
+def detectar_objetos_imagens(pasta: str, conf: float = 0.25) -> None:
+    yolo_cmd = f"yolo task=detect mode=predict model=yolov8n.pt conf={conf} source='{pasta}' save=True"
     os.system(yolo_cmd)
